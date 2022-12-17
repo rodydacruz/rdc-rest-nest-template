8296b1febde64f002c0c2db1874125c1555e2446
rodydacruz <rodylag@outlook.com>
2022-12-17 18:07:43 -0100

    update Modeks

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,19 +1,46 @@
 import { Injectable } from "@nestjs/common";
+import { ForbiddenException } from "@nestjs/common/exceptions";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 const argon = require('argon2');
 
 @Injectable({})
 export class AuthService {
-    constructor(private prisma: PrismaService) {}
-    signup(dto: AuthDto) {
+    constructor(private prisma: PrismaService) { }
+    async signup(dto: AuthDto) {
         //Generate the password
-        const hash = argon.hash(dto.password);
+        const hash = await argon.hash(dto.password);
         //save the new user
-        return{
-            sms: 'I am singupo',
+        try {
+            const user = await this.prisma.user.create(
+                {
+                    data: {
+                        email: dto.email,
+                        hash
+                    }
+                },);
+
+            delete user.hash
+            // return the saved user
+            return user;
+        } catch (error) {
+            if(error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Credentials Taken')
+                }
+            }
+            throw error;
         }
+
     }
 
-    signin() {}
+    signin() { 
+
+        //fing the user
+        //comparee password
+
+
+        //sendbacl the user
+    }
 }
