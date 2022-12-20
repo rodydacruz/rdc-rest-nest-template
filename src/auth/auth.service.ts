581af1ce070bcdd97fb140967aa0a1f99904c4d6
rodydacruz <rodylag@outlook.com>
2022-12-20 01:07:56 -0100

    add JWT Bearer

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,60 +4,81 @@ import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { JwtService } from '@nestjs/jwt/dist';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable({})
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+    constructor(
+        private prisma: PrismaService,
+        private jwt: JwtService,
+        private config: ConfigService,
+    ) { }
 
-  /**
-   *
-   * @param dto
-   * @returns
-   */
-  async signup(dto: AuthDto) {
-    //Generate the password
-    const hash = await argon.hash(dto.password);
-    //save the new user
-    try {
-      const user = await this.prisma.user.create({
-        data: {
-          email: dto.email,
-          hash,
-        },
-      });
+    /**
+     *
+     * @param dto
+     * @returns
+     */
+    async signup(dto: AuthDto) {
+        //Generate the password
+        const hash = await argon.hash(dto.password);
+        //save the new user
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                },
+            });
 
-      delete user.hash;
-      // return the saved user
-      return user;
-    } catch (error) {
-      if (error instanceof PrismaClientKnownRequestError) {
-        if (error.code === 'P2002') {
-          throw new ForbiddenException('Credentials Taken');
+            return this.signToken(user.id, user.email);
+
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Credentials Taken');
+                }
+            }
+            throw error;
         }
-      }
-      throw error;
     }
-  }
-  /**
-   *
-   * @param dto
-   */
-  async signin(dto: AuthDto) {
-    //fing the user
-    const user = await this.prisma.user.findUnique({
-      where: {
-        email: dto.email,
-      },
-    });
-    //If user does not exist trow exeption
-    if (!user) throw new ForbiddenException('Credencials incorrecy');
+    /**
+     *
+     * @param dto
+     */
+    async signin(dto: AuthDto) {
+        //fing the user
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email,
+            },
+        });
+        //If user does not exist trow exeption
+        if (!user) throw new ForbiddenException('Credencials incorrecy');
 
-    //comparee password
-    const pwMatches = await argon.verify(user.hash, dto.password);
-    if (!pwMatches) throw new ForbiddenException('Credencials incorrecy');
+        //comparee password
+        const pwMatches = await argon.verify(user.hash, dto.password);
+        if (!pwMatches) throw new ForbiddenException('Credencials incorrecy');
 
-    delete user.hash;
-    //sendbacl the user
-    return user;
-  }
+        return this.signToken(user.id, user.email);
+    }
+
+    async signToken(userId: number,
+        email: string): Promise<{ jwt_token: string }> {
+        const payload = {
+            sub: userId,
+            email
+        }
+        const secret = this.config.get('JWT_SECRET');
+
+        const token = await this.jwt.signAsync(payload, {
+            expiresIn: "30m",
+            secret: secret,
+        });
+
+        return {
+            jwt_token: token,
+        }
+    }
 }
---
+++ src/user/user.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, UseGuards, Req } from '@nestjs/common';
+import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard';
+
+@Controller('users')
+export class UserController {
+
+    @UseGuards(JwtGuard)
+    @Get('me')
+    getMe(@Req() req: Request) {
+        console.log({
+            user: req.user,
+        })
+        return 'user info';
+    }
+}
