e89ef07982ff7deb0213b630c34e401a77f97711
rodydacruz <rodylag@outlook.com>
2022-12-19 23:02:46 -0100

    npm is better than yarn adding some authentication helper libraries

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,46 +1,63 @@
-import { Injectable } from "@nestjs/common";
-import { ForbiddenException } from "@nestjs/common/exceptions";
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
-import { PrismaService } from "src/prisma/prisma.service";
-import { AuthDto } from "./dto";
-const argon = require('argon2');
+import { Injectable } from '@nestjs/common';
+import { ForbiddenException } from '@nestjs/common/exceptions';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable({})
 export class AuthService {
-    constructor(private prisma: PrismaService) { }
-    async signup(dto: AuthDto) {
-        //Generate the password
-        const hash = await argon.hash(dto.password);
-        //save the new user
-        try {
-            const user = await this.prisma.user.create(
-                {
-                    data: {
-                        email: dto.email,
-                        hash
-                    }
-                },);
+  constructor(private prisma: PrismaService) {}
 
-            delete user.hash
-            // return the saved user
-            return user;
-        } catch (error) {
-            if(error instanceof PrismaClientKnownRequestError) {
-                if (error.code === 'P2002') {
-                    throw new ForbiddenException('Credentials Taken')
-                }
-            }
-            throw error;
+  /**
+   *
+   * @param dto
+   * @returns
+   */
+  async signup(dto: AuthDto) {
+    //Generate the password
+    const hash = await argon.hash(dto.password);
+    //save the new user
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      delete user.hash;
+      // return the saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials Taken');
         }
-
+      }
+      throw error;
     }
+  }
+  /**
+   *
+   * @param dto
+   */
+  async signin(dto: AuthDto) {
+    //fing the user
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    //If user does not exist trow exeption
+    if (!user) throw new ForbiddenException('Credencials incorrecy');
 
-    signin() { 
+    //comparee password
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    if (!pwMatches) throw new ForbiddenException('Credencials incorrecy');
 
-        //fing the user
-        //comparee password
-
-
-        //sendbacl the user
-    }
+    delete user.hash;
+    //sendbacl the user
+    return user;
+  }
 }
